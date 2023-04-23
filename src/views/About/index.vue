97df9e7002cd5ab68852ab8830b33f6d2eef4f42
Sato Nao <70cojicoji@gmail.com>
2023-04-23 20:52:31 +0900

    About 画面 wip

---
+++ src/views/About/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import AboutMe from './components/AboutMe/index.vue'
+import SkillSet from './components/SkillSet/index.vue'
+import Career from './components/Career/index.vue'
+import SocialMedia from './components/SocialMedia/index.vue'
+</script>
+
+<template>
+  <div class="about-view">
+    <about-me id="aboutMe" class="content"></about-me>
+    <skill-set id="skillSet" class="content"></skill-set>
+    <career id="career" class="content"></career>
+    <social-media id="socialMedia" class="content"></social-media>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/general';
+
+.about-view {
+  margin-bottom: 2rem;
+
+  .content {
+    margin-top: 2rem;
+  }
+}
+</style>
