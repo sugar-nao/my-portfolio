bb114f21f0764764996a5e6e7edc3a99b231cae9
Sato Nao <70cojicoji@gmail.com>
2023-04-23 19:55:34 +0900

    Home 画面実装

---
+++ src/views/Home/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-view">
+    <p class="title">SATONAO</p>
+    <p class="text">Web Frontend Engineer</p>
+    <img alt="logo" src="@/assets/logo.svg" class="image" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/general';
+
+.home-view {
+  background-color: general.$top-color;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > .image {
+    width: 20rem;
+    height: 20rem;
+  }
+
+  > .title {
+    font-size: 3rem;
+  }
+
+  > .text {
+    font-size: 2rem;
+  }
+}
+</style>
