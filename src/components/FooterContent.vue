486c472ce13586e187e48b839bf61b7aeda667de
Sato Nao <70cojicoji@gmail.com>
2023-04-23 19:50:13 +0900

    FooterContent 実装

---
+++ src/components/FooterContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer-content"><p class="copyright">© 2023 Nao Sato</p></footer>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/general';
+
+.footer-content {
+  background-color: general.$accent-color;
+  display: flex;
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  > .copyright {
+    color: general.$dark-text-color;
+    font-size: 1rem;
+  }
+}
+</style>
