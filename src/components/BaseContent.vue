683a7bd5c0b6683f0ef72dcc798d5cecfdd7e4bc
Sato Nao <70cojicoji@gmail.com>
2023-04-24 23:07:29 +0900

    BaseContent 実装

---
+++ src/components/BaseContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="base-content">
+    <h1 class="title"><slot name="title"></slot></h1>
+    <div class="content"><slot name="content"></slot></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/general';
+
+.base-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > .title {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  > .content {
+    margin-top: 2rem;
+  }
+}
+</style>
--- src/views/About/components/AboutMe/index.vue
+++ src/views/About/components/AboutMe/index.vue
@@ -1,50 +1,50 @@
+<script setup lang="ts">
+import BaseContent from '@/components/BaseContent.vue'
+</script>
+
 <template>
-  <div class="about-me">
-    <h1 class="title">About Me</h1>
-    <div class="wrapper">
-      <img alt="profile" src="@/assets/logo.svg" class="image" />
-      <div class="content">
-        <div class="description">
-          こんにちは！satonaoと申します。
-          <br />東京でフロントエンドエンジニアとして働いています。<br />
-          映画が大好きなので、いつか映画に関係するサービスを作るのが夢です。
+  <base-content class="about-me">
+    <template #title>About Me</template>
+    <template #content>
+      <div class="wrapper">
+        <img alt="profile" src="@/assets/logo.svg" class="image" />
+        <div class="content">
+          <div class="description">
+            こんにちは！satonaoと申します。
+            <br />東京でフロントエンドエンジニアとして働いています。<br />
+            映画が大好きなので、いつか映画に関係するサービスを作るのが夢です。
+          </div>
+          <ul class="list">
+            <li class="item">趣味：映画鑑賞・旅行・競馬</li>
+            <li class="item">好きな食べ物：梅干し</li>
+            <li class="item">嫌いな食べ物：なし</li>
+            <li class="item">好きなお酒：ビール・日本酒・マッコリ</li>
+          </ul>
         </div>
-        <ul class="list">
-          <li class="item">趣味：映画鑑賞・旅行・競馬</li>
-          <li class="item">好きな食べ物：梅干し</li>
-          <li class="item">嫌いな食べ物：なし</li>
-          <li class="item">好きなお酒：ビール・日本酒・マッコリ</li>
-        </ul>
       </div>
-    </div>
-  </div>
+    </template>
+  </base-content>
 </template>
 
 <style lang="scss" scoped>
 @use '@/assets/general';
 
 .about-me {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  > .wrapper {
+  .wrapper {
     align-items: center;
     display: flex;
-    margin-top: 2rem;
   }
 
-  > .wrapper > .image {
+  .wrapper > .image {
     width: 15rem;
     height: 15rem;
   }
 
-  > .wrapper > .content {
+  .wrapper > .content {
     margin-left: 2rem;
   }
 
-  > .wrapper > .content > .list {
+  .wrapper > .content > .list {
     list-style-type: none;
     margin-top: 1rem;
   }
